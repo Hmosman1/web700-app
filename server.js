0a9bfe2631fe7185e5d842fbdeae0a7ab5415437
Hmosman1 <hmosman1@myseneca.ca>
2024-06-14 20:34:40 -0400

    "Student info"

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB700 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Hafsa Osman Student ID:168312239  Date: 14/06/2024
+
+********************************************************************************/
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var path = require('path');
